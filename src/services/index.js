9795ceebcf23560fe462a5e12b121408f0edd297
Sumeet Padavala <sumeet.padavala@gmail.com>
2017-04-12 10:45:53 +0530

    Remove unnceccesary sservices

--- src/services/index.js
+++ src/services/index.js
@@ -4,21 +4,10 @@ const instructorClasses = require('./instructor-classes');
 const studentClasses = require('./student-classes');
 const instructor = require('./instructor');
 const student = require('./student');
-const authentication = require('./authentication');
-const user = require('./user');
-const Sequelize = require('sequelize');
 
 module.exports = function() {
   const app = this;
 
-  const sequelize = new Sequelize(app.get('postgres'), {
-    dialect: 'postgres',
-    logging: false
-  });
-  app.set('sequelize', sequelize);
-
-  app.configure(authentication);
-  app.configure(user);
   app.configure(student);
   app.configure(instructor);
   app.configure(studentClasses);
